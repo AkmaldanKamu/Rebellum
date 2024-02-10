3de3942318b9585beb4a6e190acd83c75da60ab0
ramarfx <ramagamiing33@gmail.com>
2024-02-10 14:46:44 +0700

    add chart.js functionally

--- src/js/script.js
+++ src/js/script.js
@@ -23,15 +23,15 @@ window.onscroll = () => {
 	}
 };
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
   sections.forEach(sec => {
-	let top = window.scrollY;
-	let offset = sec.offsetTop;
-	let height = sec.offsetHeight;
-	let id = sec.getAttribute('id');
+	const top = window.scrollY;
+	const offset = sec.offsetTop;
+	const height = sec.offsetHeight;
+	const id = sec.getAttribute('id');
 	  
 	  if (top >= offset && top < offset + height) {
 		navLinks.forEach(links => {
--- src/js/three.js
+++ src/js/three.js
@@ -2,7 +2,6 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import celurit from '../assets/celurit/celurit.gltf';
-
 const canvas = document.getElementById('canvas')
 
 const renderer = new THREE.WebGLRenderer({

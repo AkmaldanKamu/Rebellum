e5064527cea1bfd7b8eba6776e08bb548e575adb
ramarfx <ramagamiing33@gmail.com>
2024-02-13 23:30:24 +0700

    add animation 3D

--- src/js/three.js
+++ src/js/three.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import celurit from '../assets/celurit/celurit.gltf';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 const canvas = document.getElementById('canvas')
 
 const renderer = new THREE.WebGLRenderer({
@@ -9,16 +9,19 @@ const renderer = new THREE.WebGLRenderer({
     alpha: true
 });
 
-const width = 300;
+const width = 500;
 const height = 300;
 renderer.setSize(width, height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+const composer = new EffectComposer(renderer);
 
 const scene = new THREE.Scene();
 renderer.setClearColor(0x000000, 0);
 // scene.background = new THREE.Color(0x5562AD);
 
 const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-camera.position.set(0, 0, 30); // Posisi kamera diperbarui untuk menghadap objek dari jarak 100 unit
+camera.position.set(0, 0, 95); // Posisi kamera diperbarui untuk menghadap objek dari jarak 100 unit
 camera.lookAt(0, 0, 0); // Kamera menghadap ke titik tengah (0, 0, 0) di scene
 
 const gridHelper = new THREE.GridHelper(30);
@@ -32,19 +35,42 @@ ambientLight.intensity = 15;
 scene.add(ambientLight)
 
 const loader = new GLTFLoader();
-loader.load('/celurit.gltf', (gltf) => {
-    gltf.scene.scale.set(10, 10, 10)
-    gltf.scene.rotation.z = Math.PI / 8
-    scene.add(gltf.scene)
+
+const celurit1 = new Promise((resolve, reject) => {
+    loader.load('/celurit.gltf', (gltf) => {
+        gltf.scene.scale.set(25, 25, 25)
+        gltf.scene.rotation.z = Math.PI / 6
+        scene.add(gltf.scene)
+        resolve(gltf.scene);
+    })
 })
+const celurit2 = new Promise((resolve, reject) => {
+    loader.load('/celurit.gltf', (gltf) => {
+        gltf.scene.scale.set(25, 25, 25)
+        gltf.scene.rotation.z = Math.PI / 6
+        gltf.scene.rotation.y = Math.PI
+        scene.add(gltf.scene)
+        resolve(gltf.scene);
+    })
+})
+
+Promise.all([celurit1, celurit2]).then(([celurit1, celurit2]) => {
+    // Kedua model telah dimuat
+    // Mulai animasi rotasi untuk kedua model di sini
+    animateRotation(celurit1);
+    animateRotation(celurit2);
+});
 
 const orbit = new OrbitControls(camera, canvas);
 orbit.enablePan = false
 orbit.enableZoom = false
 orbit.update();
 
-function animate() {
-    renderer.render(scene, camera);
+function animateRotation(object) {
+    function animate() {
+        requestAnimationFrame(animate);
+        object.rotation.y += Math.PI / 270;
+        renderer.render(scene, camera);
+    }
+    animate();
 }
-
-renderer.setAnimationLoop(animate);
